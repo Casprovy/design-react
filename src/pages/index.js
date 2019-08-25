2467f2d725c2db7cc1d2c7f0e6498459736ab335
Tomasz Gasienica-Szostak <casprovy.tgs@gmail.com>
2019-08-25 21:47:09 +0200

    working on css

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,18 +3,25 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
+import Header from "../components/header"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi Tomasz</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div>
+      <Header />
+      <div className="Hero">
+        <div className="HeroGroup"></div>
+        <SEO title="Home" />
+        <h1>Hi Tomasz</h1>
+        <p>Welcome to your new Gatsby site.</p>
+        <p>Now go build something great.</p>
+        <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+          <Image />
+        </div>
+        <Link to="/video">Watch the video</Link>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
