0eb866bf7b96b7ec22079d6d38a07dd61bf663f7
Tomasz Gasienica-Szostak <casprovy.tgs@gmail.com>
2019-09-02 21:46:40 +0200

    playing with css

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,13 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
-import "../components/header.css"
+import { Link } from 'gatsby'
+import PropTypes from 'prop-types'
+import React from 'react'
+import '../components/header.css'
 
 const Header = ({ siteTitle }) => (
   <div className="Header">
     <div className="HeaderGroup">
       <Link to="/">
-        <img width="30" src={require("../images/logo-designcode.svg")} />
+        <img width="30" src={require('../images/logo-designcode.svg')} />
       </Link>
       <Link to="/courses">Courses</Link>
       <Link to="/downloads">Downloads</Link>
